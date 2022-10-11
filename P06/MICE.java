7521a99a04da45b99d1575b1573dbd4f2af0c0d6
Andy Tieu <andytieu325@gmail.com>
2022-10-11 01:53:39 -0500

    main

--- P06/MICE.java
+++ P06/MICE.java
@@ -1,8 +1,10 @@
+import gui.MainWin;
+
 public class MICE
 {
 	public static void main(String[] args)
 	{
-		MainWin myStore = new MainWin();
+		gui.MainWin myStore = new gui.MainWin();
 		myStore.setVisible(true);
 	}
 }
